f9e61f696bf7c6af7f28e67e561be31b10defeb1
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-15 20:09:03 +0300

    Using signals input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, computed, Input, input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +9,19 @@ import { Component, Input} from '@angular/core';
 })
 
 export class UserComponent {
-@Input({required: true}) avatar!: string;
-@Input({required: true}) name!: string;
+// @Input({required: true}) avatar!: string;
+// @Input({required: true}) name!: string;
+avatar = input.required<string>();
+name = input.required<string>();
 
-get imagePath() {
-  return 'assets/users/' + this.avatar;
-}
+imagePath = computed(()=> {
+    return 'assets/users/' + this.avatar();
+
+})
+
+// get imagePath() {
+//   return 'assets/users/' + this.avatar;
+// }
 
   onSelectedUser() {
   }
