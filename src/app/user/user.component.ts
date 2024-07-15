44f2bd444806fad385f486e9203ebd8b14ff77ee
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-15 20:10:15 +0300

    Rollback to legacy angular input method

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,19 +9,12 @@ import { Component, computed, Input, input} from '@angular/core';
 })
 
 export class UserComponent {
-// @Input({required: true}) avatar!: string;
-// @Input({required: true}) name!: string;
-avatar = input.required<string>();
-name = input.required<string>();
+@Input({required: true}) avatar!: string;
+@Input({required: true}) name!: string;
 
-imagePath = computed(()=> {
-    return 'assets/users/' + this.avatar();
-
-})
-
-// get imagePath() {
-//   return 'assets/users/' + this.avatar;
-// }
+get imagePath() {
+  return 'assets/users/' + this.avatar;
+}
 
   onSelectedUser() {
   }
