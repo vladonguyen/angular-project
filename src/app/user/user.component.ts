ea6c2c855a27b825894721bd3ac309bf3c4fab45
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-18 00:01:35 +0300

    Model added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-interface User {
-  id: string;
-    avatar: string;
-    name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
