5833545977a23c0b1f58bac1952a36675f67b98a
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-24 00:31:37 +0300

    Deleting tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -42,4 +42,8 @@ export class TasksComponent {
   get selectedUserTasks(){
     return this.tasks.filter((task)=> task.userId === this.userId)
   }
+
+  onCompleteTask(id: String){
+    this.tasks = this.tasks.filter((task)=> task.id !== id)
+  }
 }
