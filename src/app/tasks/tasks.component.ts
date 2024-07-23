ec7dbcd38a3c43bb129e9e0690622af98aaf895e
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-24 01:21:17 +0300

    New task action button added

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 
@@ -13,6 +14,8 @@ import { TaskComponent } from "./task/task.component";
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -45,5 +48,9 @@ export class TasksComponent {
 
   onCompleteTask(id: String){
     this.tasks = this.tasks.filter((task)=> task.id !== id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
   }
 }
