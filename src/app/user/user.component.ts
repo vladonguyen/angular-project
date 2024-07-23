e334fa568e7b2c81590e770778d71f15e26a5407
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-24 00:19:04 +0300

    App, tasks, user components update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { User } from './user.model';
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -11,6 +11,7 @@ import { User } from './user.model';
 
 export class UserComponent {
   @Input({required: true}) user!: User;
+  @Input ({required: true}) selected!: boolean;
  
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
