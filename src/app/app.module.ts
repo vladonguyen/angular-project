2965a5f031f45dee81987084923e07a2980e9696
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-29 21:04:29 +0300

    Adding app module and testing mixing standalone and module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @NgModule({
-    declarations: []
+    declarations: [AppComponent],
+    bootstrap:[AppComponent],
+    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppModule {}
