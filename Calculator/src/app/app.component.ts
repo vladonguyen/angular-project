58c8f51e9958c308bdbfe0c0aa1907c71c06c953
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-01 00:13:47 +0300

    HTML skeleton components added

--- Calculator/src/app/app.component.ts
+++ Calculator/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestementResultComponent } from './investement-result/investement-result.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports:[HeaderComponent, UserInputComponent, InvestementResultComponent]
 })
 export class AppComponent {}
---
+++ Calculator/src/app/investement-result/investement-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investement-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './investement-result.component.html',
+  styleUrl: './investement-result.component.css'
+})
+export class InvestementResultComponent {
+
+}
