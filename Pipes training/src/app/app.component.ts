a4cde671fdb6b47b570ecbc7d9e1809f521e09ab
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-09 00:04:28 +0300

    Sort pipe

--- Pipes training/src/app/app.component.ts
+++ Pipes training/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { TempreraturePipe } from './temperature.pipe';
+import { SortPipe } from "./sort.pipe";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [DatePipe, DecimalPipe, TempreraturePipe]
+  imports: [DatePipe, DecimalPipe, TempreraturePipe, SortPipe]
 })
 export class AppComponent {
   currentDate = new Date();
@@ -21,7 +22,14 @@ export class AppComponent {
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
+  constructor(){
+    this.historicTemperatures.sort((a,b)=> a > b ? 1: -1)
+  }
+
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    // this.historicTemperatures[index] = 18;
+    const newTemps = [...this.historicTemperatures];
+    newTemps[index]=18;
+    this.historicTemperatures = newTemps;
   }
 }
