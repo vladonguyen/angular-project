e0a9cac8e773519d3586692c982760a4c403e536
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-11 23:04:29 +0300

    RxJS interval observable added

--- Rxjs/src/app/app.component.ts
+++ Rxjs/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+  interval(1000).subscribe({
+    next: (val) => console.log(val),
+  });
+}
 }
