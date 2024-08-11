51e9ea6a80223a2eba29b8beacd53b6dee84fd90
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-11 23:08:05 +0300

    DestroyRef added

--- Rxjs/src/app/app.component.ts
+++ Rxjs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
 
 
@@ -8,9 +8,14 @@ import { interval } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
 ngOnInit(): void {
-  interval(1000).subscribe({
+  const subscription = interval(1000).subscribe({
     next: (val) => console.log(val),
   });
+
+  this.destroyRef.onDestroy(()=> {
+    subscription.unsubscribe();
+  })
 }
 }
