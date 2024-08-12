a99a97037e8ed28d5cdac0e2f8a44fbcff677f85
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-12 14:19:57 +0300

    RxJS observables updated

--- Rxjs/src/app/app.component.ts
+++ Rxjs/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { interval, map, Observable } from 'rxjs';
 
 
 @Component({
@@ -7,29 +8,58 @@ import { interval, map } from 'rxjs';
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      // subscriber.error();
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emmiting new value...');
+      subscriber.next({ message: 'New Value' });
+      timesExecuted++;
+    }, 2000)
+  })
   private destroyRef = inject(DestroyRef);
 
-constructor(){
-  effect(()=>{
-    console.log(`Clicked button ${this.clickCount()} times.`)
-  })
-}
+  constructor() {
+    // effect(()=>{
+    //   console.log(`Clicked button ${this.clickCount()} times.`)
+    // })
+  }
 
-ngOnInit(): void {
-  // const subscription = interval(1000).pipe(
-  //   map((val) => val * 2)
-  // ).subscribe({
-  //   next: (val) => console.log(val),
-  // });
+  ngOnInit(): void {
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
 
-  // this.destroyRef.onDestroy(()=> {
-  //   subscription.unsubscribe();
-  // })
-}
+    // this.destroyRef.onDestroy(()=> {
+    //   subscription.unsubscribe();
+    // })
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('COMPLETED!'),
+      error: (err)=> console.log(err)
+    });
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${this.clickCount()} times.`)
+    });
 
-onClick() {
-this.clickCount.update(prevCount => prevCount + 1)
-}
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
+  }
 }
