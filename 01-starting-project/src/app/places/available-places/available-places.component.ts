5058a7b7f86517e0847c0730c7fbf71ce96b7cf0
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-14 23:44:42 +0300

    HTTP configuration

--- 01-starting-project/src/app/places/available-places/available-places.component.ts
+++ 01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +12,20 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+ ngOnInit(): void {
+  const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
+    next:(resData)=> {
+      console.log(resData.places);
+    }
+   });
+
+   this.destroyRef.onDestroy(()=> {
+subscription.unsubscribe();
+   });
+ }
 }
