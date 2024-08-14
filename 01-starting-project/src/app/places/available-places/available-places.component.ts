e04eda49821286458217714caaad451eeba24d6b
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-15 00:42:08 +0300

    Error catch and display added

--- 01-starting-project/src/app/places/available-places/available-places.component.ts
+++ 01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,19 +14,43 @@ import { HttpClient } from '@angular/common/http';
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal('');
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
- ngOnInit(): void {
-  const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
-    next:(resData)=> {
-      console.log(resData.places);
-    }
-   });
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places), 
+        catchError((error)=> {  
+      console.log(error);
 
-   this.destroyRef.onDestroy(()=> {
-subscription.unsubscribe();
-   });
- }
+        return  throwError(
+          ()=> 
+          new Error(
+            "something went wrong fetching available places. Try again later"
+          )
+        );
+      })
+    )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places)
+        },
+        error: (error: Error)=>{
+          this.error.set(error.message);
+        },
+        complete: ()=>{
+          this.isFetching.set(false);
+        }
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
