97b1dd23a66d063f061b2c39e0fef0e02360b5be
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-15 00:43:15 +0300

    Backed err recovery

--- 01-starting-project/backend/app.js
+++ 01-starting-project/backend/app.js
@@ -20,7 +20,6 @@ app.use((req, res, next) => {
 
 app.get("/places", async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  return res.status(500).json();
 
   const fileContent = await fs.readFile("./data/places.json");
 
