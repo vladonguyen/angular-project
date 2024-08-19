fd1611378dd31d538987531d9d1238110ef9aa11
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-08-20 00:28:51 +0300

    Not found comp added

--- Task manager 3 Routing/src/app/app.routes.ts
+++ Task manager 3 Routing/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { TasksComponent } from "./tasks/tasks.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
     {
@@ -16,12 +17,22 @@ export const routes: Routes = [
         component: UserTasksComponent,
         children: [
             {
+                path: '',
+                redirectTo: 'tasks',
+                pathMatch: 'full'
+            },
+
+            {
                 path: 'tasks',
                 component: TasksComponent
             },
             {
                 path: 'tasks/new',
                 component: NewTaskComponent
+            },
+            {
+                path: '**',
+                component: NotFoundComponent
             }
         ]
     }
