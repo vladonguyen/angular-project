66e9ca63e471d4bd6dd302c090604506cefcb481
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-01 21:32:41 +0300

    Using query parameters for data manipulation

--- Task manager 3 Routing/src/app/tasks/tasks.component.ts
+++ Task manager 3 Routing/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -15,16 +15,29 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   // order = input<'asc' | "desc" | undefined>();
-  order?: 'asc' | 'desc';
+  // order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
+
   private tasksService = inject(TasksService)
   userTasks = computed (()=> 
-    this.tasksService.allTasks().filter(task => task.userId === this.userId()));
+    this.tasksService
+  .allTasks()
+  .filter(task => task.userId === this.userId())
+  .sort((a,b)=>{
+    if(this.order() === 'desc'){
+      return a.id > b.id ? -1 : 1;
+    }else {
+      return a.id > b.id ? 1: -1;
+    }
+  })
+
+);
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
    const subscription = this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order'],
+      next: (params) => (this.order.set (params['order'])),
     });
 
     this.destroyRef.onDestroy(()=> subscription.unsubscribe());
