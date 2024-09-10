a8a5c2a68bfc367d0489a0966777479ddfebf920
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-11 00:12:41 +0300

    Lazy loading service

--- Task manager 4 Lazy loading/src/app/app.routes.ts
+++ Task manager 4 Lazy loading/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { inject } from '@angular/core';
 
-import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
   UserTasksComponent,
@@ -30,7 +29,8 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: userRoutes,
+    loadChildren: ()=> 
+      import('./users/users.routes').then(mod=>mod.routes),
     canMatch: [dummyCanMatch],
     data: {
       message: 'Hello!',
--- Task manager 4 Lazy loading/src/app/users/users.routes.ts
+++ Task manager 4 Lazy loading/src/app/users/users.routes.ts
@@ -1,50 +1,36 @@
-import { ResolveFn, Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
 
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { inject } from '@angular/core';
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { TasksService } from '../tasks/tasks.service';
-
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
 
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: ()=> 
-      import('../tasks/tasks.component').then((mod)=> mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
-  },
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent, 
+        // <your-domain>/users/<uid>/tasks
+        // loadComponent: ()=> 
+        //   import('../tasks/tasks.component').then((mod)=> mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  }
 ];
