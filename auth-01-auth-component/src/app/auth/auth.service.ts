5d8424066ba9ab79b7827ac5af8d3828ad277ec1
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-14 17:30:36 +0300

    Custom error message added

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 
 interface AuthResponseData {
@@ -25,6 +27,17 @@ export class AuthService {
             }
 
         )
+        .pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred!';
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already!'   
+            }
+            return throwError(errorMessage);
+        }))
     }
 
 }
