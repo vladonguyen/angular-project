0f74b7a6e37e738b58bea0ca0cd5148a5b08735f
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-14 21:05:20 +0300

    Login Error Handling

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -52,4 +52,5 @@ export class AuthComponent {
 
     form.reset();
   }
+  
 }
--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
@@ -28,21 +28,11 @@ export class AuthService {
             }
 
         )
-        .pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred!';
-            if(!errorRes.error || !errorRes.error.error){
-                return throwError(errorMessage);
-            }
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email exists already!'   
-            }
-            return throwError(errorMessage);
-        }))
+            .pipe(catchError(this.handleError))
     }
 
-    login(email: string, password: string){
-   return this.http.post<AuthResponseData>(
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
             `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
             {
                 email,
@@ -50,6 +40,27 @@ export class AuthService {
                 returnSecureToken: true
             }
         )
+            .pipe(catchError(this.handleError));
     }
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!';
 
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already!';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist!';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct!';
+                break;
+            case 'INVALID_LOGIN_CREDENTIALS':
+                errorMessage = 'Invalid username or password!';
+        }
+        return throwError(errorMessage);
+    }
 }
