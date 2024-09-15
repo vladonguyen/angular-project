3286980bf7f7f9f1376140d588128f08603781f7
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-15 20:58:18 +0300

    localstorage login saved

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -68,8 +68,20 @@ export class AuthService {
             }));
     }
 
+    autoLogin(){
+       const userData: {email: string, id: string, _token: string, _tokenExpirationDate: string} = JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id,userData._token, new Date(userData._tokenExpirationDate));
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+        }
+    }
+
     logout(){
-        console.log('LOGOUT')
         this.user.next(null);
         this.router.navigate(['/auth']);
     }
@@ -82,6 +94,7 @@ export class AuthService {
             token,
             expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
