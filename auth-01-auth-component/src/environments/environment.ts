4d3a8b533a67ad5d793e13544000538283b4b0c3
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 00:24:05 +0300

    Update environment.ts

--- auth-01-auth-component/src/environments/environment.ts
+++ auth-01-auth-component/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiKey: 'AIzaSyBvJbCuM50qq55gGzV-ZKvjsW0rY_ZdaKM'
 };
 
 /*
