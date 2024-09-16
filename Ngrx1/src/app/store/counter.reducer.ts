9a1e3dd37e109a06d4d433e3f19c082975e772cf
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 12:09:59 +0300

    Ngrx setup and initial reducer added

---
+++ Ngrx1/src/app/store/counter.reducer.ts
@@ -0,0 +1,9 @@
+import { createReducer } from "@ngrx/store";
+
+const initialState = 0;
+
+// export const counterReducer = createReducer(initialState);
+
+export function counterReducer(state = initialState){
+    return state;
+}
