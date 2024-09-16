6fa332e4f0ef62ab08de2eb0e8e351668b9f519b
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 15:29:51 +0300

    Dispach increment action

--- Ngrx1/src/app/counter-controls/counter-controls.component.ts
+++ Ngrx1/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-import { CounterService } from '../counter.service';
+import { increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +10,12 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment())
   }
 
   decrement() {
-    this.counterService.decrement();
   }
 }
--- Ngrx1/src/app/store/counter.reducer.ts
+++ Ngrx1/src/app/store/counter.reducer.ts
@@ -1,9 +1,12 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.actions";
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+    initialState, 
+    on(increment, (state)=> state + 1));
 
-export function counterReducer(state = initialState){
-    return state;
-}
+// export function counterReducer(state = initialState){
+//     return state;
+// }
