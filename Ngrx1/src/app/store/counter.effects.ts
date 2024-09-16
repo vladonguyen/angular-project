89b6d2c2cdd6b6796305d7299ffb27858ed0e558
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 23:44:50 +0300

    Counter.effects.ts and main.ts updates

--- Ngrx1/src/app/store/counter.effects.ts
+++ Ngrx1/src/app/store/counter.effects.ts
@@ -1,7 +1,9 @@
 import { act, Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
 import { tap } from "rxjs";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CounterEffects {
     saveCount = createEffect(()=> this.actions$.pipe(
         ofType(increment, decrement), // the actions will continue if they are of these types
--- Ngrx1/src/main.ts
+++ Ngrx1/src/main.ts
@@ -4,7 +4,8 @@ import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
 import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './app/store/counter.effects';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore({ counter: counterReducer }), provideEffects()]
+    providers: [provideStore({ counter: counterReducer }), provideEffects([CounterEffects])]
 });
