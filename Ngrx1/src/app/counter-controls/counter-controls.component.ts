daaaa6680257715bd351290657587d6779a4e62b
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 18:11:47 +0300

    Restore actions code

--- Ngrx1/src/app/counter-controls/counter-controls.component.ts
+++ Ngrx1/src/app/counter-controls/counter-controls.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { increment } from '../store/counter.actions';
 
-import { IncrementAction } from '../store/counter.actions';
+// import { IncrementAction } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -13,7 +14,9 @@ export class CounterControlsComponent {
   constructor(private store: Store) {}
 
   increment() {
-    this.store.dispatch(new IncrementAction (2))
+    this.store.dispatch(increment({value: 2}));
+    // this.store.dispatch(new IncrementAction (2))
+
   }
 
   decrement() {
--- Ngrx1/src/app/store/counter.reducer.ts
+++ Ngrx1/src/app/store/counter.reducer.ts
@@ -1,21 +1,21 @@
 import { Action, createReducer, on } from "@ngrx/store";
-import { CounterActions, IncrementAction } from "./counter.actions";
-// import { increment } from "./counter.actions";
+// import { CounterActions, IncrementAction } from "./counter.actions";
+import { increment } from "./counter.actions";
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(
-//     initialState, 
-//     on(increment, (state, action)=> state + action.value));
+export const counterReducer = createReducer(
+    initialState, 
+    on(increment, (state, action)=> state + action.value));
 
 
 //alternative avoiding createReducer with vanilla JS
 
-export const INCREMENT = '[Counter] Increment'
+// export const INCREMENT = '[Counter] Increment'
 
-export function counterReducer(state = initialState, action: CounterActions | Action){
-    if(action.type === INCREMENT){
-        return state + (action as IncrementAction).value;
-    }
-    return state;
-}
+// export function counterReducer(state = initialState, action: CounterActions | Action){
+//     if(action.type === INCREMENT){
+//         return state + (action as IncrementAction).value;
+//     }
+//     return state;
+// }
