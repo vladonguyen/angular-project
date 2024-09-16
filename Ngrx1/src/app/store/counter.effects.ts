c9a10dcffe95e16452f4f3542e724d96b594b5c5
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 23:52:57 +0300

    Update to write to localstorage the latest counter value

--- Ngrx1/src/app/store/counter.effects.ts
+++ Ngrx1/src/app/store/counter.effects.ts
@@ -1,18 +1,22 @@
 import { act, Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+
+import { selectCount } from "./counter.selector";
 
 @Injectable()
 export class CounterEffects {
     saveCount = createEffect(()=> this.actions$.pipe(
         ofType(increment, decrement), // the actions will continue if they are of these types
-        tap((action)=> {
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter])=> {
             console.log(action);
-            localStorage.setItem('count', action.value.toString());
+            localStorage.setItem('count', counter.toString());
         })
 
     ), {dispatch: false});
 
-    constructor(private actions$: Actions){}
+    constructor(private actions$: Actions, private store: Store<{counter: number}>){}
 }
