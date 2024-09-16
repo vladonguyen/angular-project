2ea1165f53c1dad14ab9b253e38cfb2bac799315
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-09-16 17:16:41 +0300

    Attaching data to increment action

--- Ngrx1/src/app/store/counter.actions.ts
+++ Ngrx1/src/app/store/counter.actions.ts
@@ -1,5 +1,6 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 
 export const increment = createAction(
-    '[Counter] Increment' //in the square breket is is announced for which feature it is
-)
+    '[Counter] Increment', //in the square breket is is announced for which feature it is
+    props<{value: number}>()
+);
